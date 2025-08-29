9800615b755a5114d265f3b545d0a8c96ad09763
vancy fernandes <vancyferns20@gmail.com>
2025-08-29 19:25:16 +0000

    added link in config file

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,3 @@
 // export const API_BASE_URL = "https://silver-winner-qwjq7r76wqg2xwj7-5000.app.github.dev/";
 // export const API_BASE_URL ="http://127.0.0.1:5000";
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:5000" || "https://moodify-4-scpi.onrender.com" ;
+export const API_BASE_URL = "https://moodify-4-scpi.onrender.com" ;
