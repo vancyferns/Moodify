8fc00d80e5056870e539cdf568302140229c80f8
vancy fernandes <vancyferns20@gmail.com>
2025-08-30 13:57:17 +0000

    added correct backend url to frontend

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,3 @@
 // export const API_BASE_URL = "https://silver-winner-qwjq7r76wqg2xwj7-5000.app.github.dev/";
 // export const API_BASE_URL ="http://127.0.0.1:5000";
-export const API_BASE_URL = "https://huggingface.co/spaces/TARAMALIK16/moodify_model" ;
+export const API_BASE_URL = "https://taramalik16-moodify-model.hf.space" ;
