1a9263461935183750a3a5f402657bb63045d8f4
vancy fernandes <vancyferns20@gmail.com>
2025-09-03 20:17:05 +0000

    added new textmodel link

--- client/src/components/EmotionQuestionnaire.jsx
+++ client/src/components/EmotionQuestionnaire.jsx
@@ -106,7 +106,7 @@ function EmotionQuestionnaire() {
     setError(null);
     setLoadingMessage('Analyzing your emotions with AI...');
 
-    const apiUrl = 'https://taramalik16-moodify-textmodel.hf.space/predict';
+    const apiUrl = 'https://8000-dep-01k48ja7x1nky2k987xrt9xkac-d.cloudspaces.litng.ai/predict';
     const payload = { responses: answeredResponses };
 
     try {
